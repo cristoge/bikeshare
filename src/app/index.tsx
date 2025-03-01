afe7e7d7a96da3f752484b31ca171f3fdf6cb2c1
“Cristoge” <cristo.g@outlook.es>
2025-03-01 04:36:25 +0100

    Creada la estructura de carpetas

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AppRegistry, Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{color:'orange'}}>Hello World!!!</Text>
+    </View>
+  );
+};
+
+export default App;
---
+++ src/constants/messages.ts
@@ -0,0 +1,6 @@
+export const Messages = {
+  LOGIN_SUCCESS: 'Inicio de sesión exitoso!',
+  LOGIN_ERROR: 'Hubo un error al intentar iniciar sesión. Intenta nuevamente.',
+  REGISTER_SUCCESS: 'Registro completado con éxito.',
+  REGISTER_ERROR: 'No se pudo completar el registro. Intenta nuevamente.',
+};
