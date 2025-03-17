72d9fed58839b148264c2eeb4d2e408ff116746a
“Cristoge” <cristo.g@outlook.es>
2025-03-17 18:50:02 +0100

    tipos agregados

--- src/screens/TestScreen.tsx
+++ src/screens/TestScreen.tsx
@@ -1,12 +1,44 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
-import { getBikes } from '../services/bike';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, FlatList, StyleSheet } from 'react-native';
+import { getUsers } from '../services/user';
+import { User } from '../types/dataTypes';
+
 const TestScreen = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  const fetchUsers = async () => {
+    const data = await getUsers();
+    if (data) {
+      setUsers(data);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
-    <View> 
-      <Button title="Obtener Usuarios" onPress={getBikes} />
+    <View>
+      <Button title="Obtener Usuarios" onPress={fetchUsers} />
+      <FlatList
+        data={users}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View>
+            <Text style={styles.orangeText}>{item.id}</Text>
+            <Text>{item.role}</Text>
+            <Text>{item.email}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  orangeText: {
+    color: 'orange',
+  },
+});
 
 export default TestScreen;
