83ea5651a6af4f948b42ff85f9537daadf71334e
Tony <tonyleonel.reyna.pinto-bazurco@prateducacio.com>
2025-05-12 21:08:41 +0200

    añadida imagen para pantalla

--- src/screens/Info/comingSoon.tsx
+++ src/screens/Info/comingSoon.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Image } from 'react-native';
 
 const commingSoon = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Coming Soon</Text>
-    </View>
-  );
+  <View style={styles.container}>
+    <Image 
+      source={require('../assets/icon.png')} 
+      style={styles.image}
+    />
+    <Text style={styles.text}>Coming Soon</Text>
+  </View>
+);
 };
 
 const styles = StyleSheet.create({
@@ -20,6 +25,11 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
   },
+  image: {
+  width: 200,
+  height: 200,
+  marginBottom: 30,
+}
 });
 
 export default commingSoon;
